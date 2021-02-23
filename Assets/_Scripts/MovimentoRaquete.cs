98ce877f095f107280e5cc8284e0b5b3f7813d16
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-23 12:05:26 -0300

    Add movement

---
+++ Assets/_Scripts/MovimentoRaquete.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoRaquete : MonoBehaviour
+{
+    // Add slider on Unity's UI.
+    [Range(1, 10)]
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(inputX, 0, 0) * Time.deltaTime * speed;
+    }
+}
