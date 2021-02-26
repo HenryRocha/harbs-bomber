5727422b6d8d910a79335a230912bdfc944f9f25
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-26 14:30:19 -0300

    Add main menu scene, with working buttons

---
+++ Assets/_Scripts/MainMenuLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
