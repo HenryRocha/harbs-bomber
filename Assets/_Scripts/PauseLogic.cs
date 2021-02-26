62db4c87a1c2b75dee79ce3a50b99f4b0f87ab7f
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-26 15:09:03 -0300

    Add Pause Menu

---
+++ Assets/_Scripts/PauseLogic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseLogic : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pausePanelUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Pressed ESC!");
+
+            if (GameIsPaused)
+            {
+                Debug.Log("Paused!");
+                ResumeGame();
+            }
+            else
+            {
+                Debug.Log("Continued!");
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pausePanelUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+
+    }
+
+    void ResumeGame()
+    {
+        pausePanelUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+}
