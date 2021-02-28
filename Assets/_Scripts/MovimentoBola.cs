60d14851194f182a8ff2a67798e531044d56ffcd
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-28 12:14:39 -0300

    Add ball reset when it hits bottom wall

--- Assets/_Scripts/MovimentoBola.cs
+++ Assets/_Scripts/MovimentoBola.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovimentoBola : MonoBehaviour
 {
     [Range(1, 15)]
-    public float baseSpeed = 5.0f;
+    public float baseSpeed = 500.0f;
 
     private Rigidbody2D rb2d;
     
@@ -17,20 +17,33 @@ public class MovimentoBola : MonoBehaviour
     {
         // Get the components rigid body.
         rb2d = GetComponent<Rigidbody2D>();
-
-        // Set the components initial speed.
-        Vector2 speed = rb2d.velocity;
-        speed.x = -baseSpeed;
-        speed.y = baseSpeed;
-        rb2d.velocity = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!gameStarted)
+        {   
+            transform.position = ballDefaultPosition.position;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && !gameStarted) {
             gameStarted = true;
-            transform.position = ballDefaultPosition.position;
+            rb2d.AddForce(Vector2.up * baseSpeed);
+        }
+    }
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "BottomWall")
+        {
+            gameStarted = false;
+            rb2d.velocity = Vector2.zero;
         }
     }
 }
