177c01a3063f3b3bd2d39018098ee09e6fd5139d
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-28 18:08:10 -0300

    Rework brick sprites and spawning

---
+++ Assets/_Scripts/BrickLogic.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickLogic : MonoBehaviour
+{
+    // Reference to the PointsUI class.
+    public PointsUI pointsUI;
+
+    // Reference to the Sprite Rendender, to enable sprite changing through scripts.
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    // List of sprites.
+    [SerializeField]
+    private Sprite[] sprites = new Sprite[10];
+
+    // Amount of hitpoints the brick has by default.
+    public int hitpoints = 1;
+
+    // Variable to store the starting amount of hitpoints.
+    public int startingHitpoints;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        spriteRenderer.sprite = sprites[hitpoints];
+        startingHitpoints = hitpoints;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        hitpoints -= 1;
+        pointsUI.UpdatePoints(+20);
+
+        if (hitpoints < 0)
+        {
+            pointsUI.UpdatePoints(+100 * startingHitpoints);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            spriteRenderer.sprite = sprites[hitpoints];
+        }
+    }
+}
---
+++ Assets/_Scripts/BrickSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawner : MonoBehaviour
+{
+    // Reference to the Brick prefab.
+    [SerializeField]
+    private GameObject BrickPrefab;
+
+    // Reference to the PointsUI class.
+    [SerializeField]
+    private PointsUI pointsUI;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Vector3 position = new Vector3(-8.5f + 1.7f * i, 0 + 0.8f * j);
+                GameObject newBrick = Instantiate(BrickPrefab, position, Quaternion.identity, transform);
+                newBrick.GetComponent<BrickLogic>().pointsUI = pointsUI;
+                newBrick.GetComponent<BrickLogic>().hitpoints = j * 2 - 1;
+            }
+        }
+    }
+}
