0b2a2e4a6655dd174d7f0ef8104e05be8cf210d0
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-28 14:27:45 -0300

    Add YouLose menu and its logic

---
+++ Assets/_Scripts/YouLoseMenuLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouLoseMenuLogic : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
