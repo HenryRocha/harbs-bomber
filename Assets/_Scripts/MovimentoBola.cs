35deea6d0a4048e6e71977dc25fc40a9e3b94259
Henry Rocha <henryfr1@al.insper.edu.br>
2021-02-28 12:01:38 -0300

    Remove ball-screen logic, add empty objects

--- Assets/_Scripts/MovimentoBola.cs
+++ Assets/_Scripts/MovimentoBola.cs
@@ -9,22 +9,12 @@ public class MovimentoBola : MonoBehaviour
 
     private Rigidbody2D rb2d;
     
-    private Vector2 screenBounds;
-    private float objectWidth;
-    private float objectHeight;
     private bool gameStarted = false;
     public Transform ballDefaultPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get the screen boundaries.
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        
-        // Get the components dimensions.
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
-
         // Get the components rigid body.
         rb2d = GetComponent<Rigidbody2D>();
 
@@ -38,44 +28,9 @@ public class MovimentoBola : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !gameStarted) {
             gameStarted = true;
-        }
-
-        if (!gameStarted) 
-        {
             transform.position = ballDefaultPosition.position;
-        }
-        else
-        {
-
-            Vector3 currentPos = transform.position;
-            Vector2 speed = rb2d.velocity;
-            
-            // Right side
-            if (currentPos.x > screenBounds.x - objectWidth) {
-                speed.x = -baseSpeed;
-            }
-
-            // Left side
-            if (currentPos.x < - screenBounds.x + objectWidth) {
-                speed.x = baseSpeed;
-            }
-
-            // Top side
-            if (currentPos.y > screenBounds.y - objectHeight) {
-                speed.y = -baseSpeed;
-            }
-
-            // Bottom side
-            if (currentPos.y < - screenBounds.y + objectHeight) {
-                speed.x = 0;
-                speed.y = 0;
-                rb2d.velocity = speed;
-                return;
-            }
-
-            rb2d.velocity = speed;
         }
     }
 }
